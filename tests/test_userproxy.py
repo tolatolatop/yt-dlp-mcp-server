9f4920d22fb38d98d560d23e2287e234e853f141
tolatolatop <tolatolatop@gmail.com>
2025-07-27 16:07:55 +0000

    feat: 修正格式错误

--- tests/test_userproxy.py
+++ tests/test_userproxy.py
@@ -4,5 +4,5 @@ from src.yt_dlp_server.userproxy import get_cookies
 
 @pytest.mark.asyncio
 async def test_get_cookies(cache):
-    cookies = await get_cookies("test_user", ".baidu.com")
-    cache.set("test_user", cookies.model_dump(mode="json"))
+    cookies = await get_cookies("test_user", ".youtube.com")
+    cache.set("test_user-cookies-json", cookies.model_dump(mode="json"))
